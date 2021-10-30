88fab49c439c6add8e108a02c9f2348af0d201d1
kinor <kinoarose@gmail.com>
2021-10-30 18:50:32 -0400

    finish the black hole block setup for the game

--- src/en3d/blocks/BlackHole.hx
+++ src/en3d/blocks/BlackHole.hx
@@ -1,8 +1,13 @@
 package en3d.blocks;
 
+import h3d.scene.Mesh;
 import h3d.prim.Primitive;
 import h3d.scene.Object;
 
+/**
+ * Black hole element, sucks in any block below it and deletes it from
+ * the game world on impact. Also sucks in the player as well.
+ */
 class BlackHole extends Block {
   public function new(x:Int, y:Int, z:Int) {
     super(x, y, z);
@@ -10,5 +15,7 @@ class BlackHole extends Block {
 
   override function setBody(prim:Primitive, root:Object) {
     super.setBody(prim, root);
+    var mesh:Mesh = cast body;
+    mesh.material.color.setColor(0xff00aa);
   }
 }
